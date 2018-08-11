a637960bb17d321798911df4310ac43655c791e5
LeftRight92 <Michael_Bohea@msn.com>
2018-08-11 17:05:09 +0100

    Adjacency code got lost for some reason

--- Assets/Scripts/ShipBuilder/ShipGrid.cs
+++ Assets/Scripts/ShipBuilder/ShipGrid.cs
@@ -12,23 +12,38 @@ namespace LD42.Scripts.ShipBuilder {
 		}
 
 		public List<ShipComponent> GetAdjacentComponents(ShipComponent component) {
-			List<ShipComponent> adjacents = new List<ShipComponent>();
-			GetOccupiedTiles(component);
-			return null;
+			List<IntPair> adjacentTiles = GetAdjacentTiles(component);
+			return components.Where(i => GetOccupiedTiles(i).Intersect(adjacentTiles).Any()).ToList();
 		}
 
-		public List<IntPair> GetOccupiedTiles(ShipComponent component) {
-			List<IntPair> tiles = new List<IntPair>();
-			foreach(IntPair i in component.Type.shape) {
-				tiles.Add(i.Rotate(locations[component].rotation))
-			}
+		private List<IntPair> GetOccupiedTiles(ShipComponent component) {
+			Rotation rotation = locations[component].rotation;
+			return component.Type.shape.Select(i => i.Rotate(rotation)).ToList<IntPair>();
+		}
+
+		private List<IntPair> GetAdjacentTiles(ShipComponent component) {
+			return GetAdjacentTiles(GetOccupiedTiles(component));
+		}
+
+		private List<IntPair> GetAdjacentTiles(List<IntPair> occupiedTiles) {
+			return occupiedTiles.SelectMany(i => GetAdjacentTiles(i))
+				.Where(i => !occupiedTiles.Contains(i))
+				.Distinct()
+				.ToList<IntPair>();
+		}
+
+		private List<IntPair> GetAdjacentTiles(IntPair i) {
+			return new List<IntPair> {
+				new IntPair(i.x + 1, i.y),
+				new IntPair(i.x - 1, i.y),
+				new IntPair(i.x, i.y + 1),
+				new IntPair(i.x, i.y - 1),
+			};
 		}
 
 		public class LocationInformation {
 			public IntPair location;
 			public Rotation rotation;
 		}
-
-		
 	}
 }
