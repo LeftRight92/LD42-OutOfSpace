05fed656fc50553b3618901f7d53edad8abb7dd2
DR Renata Retkute <plzrr@nas10-241-77-85.york.ac.uk>
2018-08-12 06:12:35 +0100

    Maybe now?

--- Assets/Scripts/World/ShipControl.cs
+++ Assets/Scripts/World/ShipControl.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LD42.Scripts.Configuration;
+using LD42.Scripts.World;
 
 public class ShipControl : MonoBehaviour {
-    public float speedHor = 0.5f, speedVert = 1;
-    public GameObject cannonFire;
-    public float cannonCooldown = 0.05f;
+    public float speedHor = 5, speedVert = 5;
+    //public GameObject cannonFire;
+    //public float cannonCooldown = 0.05f;
+    public Weapon cannons;
 
     private SpriteRenderer renderer;
     private Rigidbody2D rigidbody;
     private BoxCollider2D collider;
     private Camera camera;
 
-    private Vector3 relCannonPos;
+    //private Vector3 relCannonPos;
     private float cannonCountdown = 0;
 
     void Start () {
@@ -21,11 +24,16 @@ public class ShipControl : MonoBehaviour {
         collider = gameObject.GetComponent<BoxCollider2D>();
         camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
 
-        relCannonPos = new Vector3(
+        Vector3 relCannonPos = new Vector3(
             renderer.bounds.size.x * 7.5f/32, 
             renderer.bounds.size.y * 5.5f/32,
             0
         );
+
+        //cannons = Weapon.New(gameObject, WeaponType.CANNON, Resources.Load<GameObject>("Prefabs/CannonFire"), 
+                             //new List<Vector3>{relCannonPos, new Vector3(-relCannonPos.x, relCannonPos.y, 0)},
+                             //simultaneousFire:false);
+
 	}
 	
 	void Update () {
@@ -34,17 +42,8 @@ public class ShipControl : MonoBehaviour {
             Input.GetAxis("Vertical") * speedVert
         ));
 
-        if(cannonCountdown > 0){
-            cannonCountdown -= Time.deltaTime;
-        }
-        if(Input.GetButton("Fire1") && cannonCountdown<=0){
-            GameObject fired = Instantiate(cannonFire);
-            Physics2D.IgnoreCollision(collider, fired.GetComponent<BoxCollider2D>());
-            float firedOffset = fired.GetComponent<SpriteRenderer>().bounds.extents.y;
-            fired.transform.position = gameObject.transform.position + relCannonPos + 
-                new Vector3(0, firedOffset, 0); 
-                
-            cannonCountdown = cannonCooldown;
+        if(Input.GetButton("Fire1")){
+            cannons.AttemptFire();
         }
 	}
 }
--- Assets/Scripts/World/WeaponEditor.cs
+++ Assets/Scripts/World/WeaponEditor.cs
@@ -13,16 +13,16 @@ public class WeaponEditor : Editor
     enum displayFieldType { DisplayAsAutomaticFields, DisplayAsCustomizableGUIFields }
     displayFieldType DisplayFieldType;
 
-    CustomList t;
+    Weapon t;
     SerializedObject GetTarget;
     SerializedProperty ThisList;
     int ListSize;
 
     void OnEnable()
     {
-        t = (CustomList)target;
+        t = (Weapon)target;
         GetTarget = new SerializedObject(t);
-        ThisList = GetTarget.FindProperty("MyList"); // Find the List in our script and create a refrence of it
+        ThisList = GetTarget.FindProperty("fireLocations"); // Find the List in our script and create a refrence of it
     }
 
     public override void OnInspectorGUI()
@@ -31,42 +31,12 @@ public class WeaponEditor : Editor
 
         GetTarget.Update();
 
-        //Choose how to display the list<> Example purposes only
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        DisplayFieldType = (displayFieldType)EditorGUILayout.EnumPopup("", DisplayFieldType);
 
-        //Resize our list
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.LabelField("Define the list size with a number");
-        ListSize = ThisList.arraySize;
-        ListSize = EditorGUILayout.IntField("List Size", ListSize);
-
-        if (ListSize != ThisList.arraySize)
-        {
-            while (ListSize > ThisList.arraySize)
-            {
-                ThisList.InsertArrayElementAtIndex(ThisList.arraySize);
-            }
-            while (ListSize < ThisList.arraySize)
-            {
-                ThisList.DeleteArrayElementAtIndex(ThisList.arraySize - 1);
-            }
-        }
-
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.LabelField("Or");
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-
-        //Or add a new item to the List<> with a button
-        EditorGUILayout.LabelField("Add a new item with a button");
+        //DisplayFieldType = (displayFieldType)EditorGUILayout.EnumPopup("", DisplayFieldType);
 
         if (GUILayout.Button("Add New"))
         {
-            t.MyList.Add(new CustomList.MyClass());
+            t.fireLocations.Add(new Vector3());
         }
 
         EditorGUILayout.Space();
@@ -77,53 +47,16 @@ public class WeaponEditor : Editor
         for (int i = 0; i < ThisList.arraySize; i++)
         {
             SerializedProperty MyListRef = ThisList.GetArrayElementAtIndex(i);
-            SerializedProperty MyInt = MyListRef.FindPropertyRelative("AnInt");
-            SerializedProperty MyFloat = MyListRef.FindPropertyRelative("AnFloat");
             SerializedProperty MyVect3 = MyListRef.FindPropertyRelative("AnVector3");
-            SerializedProperty MyGO = MyListRef.FindPropertyRelative("AnGO");
-            SerializedProperty MyArray = MyListRef.FindPropertyRelative("AnIntArray");
 
 
             // Display the property fields in two ways.
 
-            if (DisplayFieldType == 0)
-            {// Choose to display automatic or custom field types. This is only for example to help display automatic and custom fields.
-                //1. Automatic, No customization <-- Choose me I'm automatic and easy to setup
-                EditorGUILayout.LabelField("Automatic Field By Property Type");
-                EditorGUILayout.PropertyField(MyGO);
-                EditorGUILayout.PropertyField(MyInt);
-                EditorGUILayout.PropertyField(MyFloat);
-                EditorGUILayout.PropertyField(MyVect3);
 
-                // Array fields with remove at index
-                EditorGUILayout.Space();
-                EditorGUILayout.Space();
-                EditorGUILayout.LabelField("Array Fields");
-
-                if (GUILayout.Button("Add New Index", GUILayout.MaxWidth(130), GUILayout.MaxHeight(20)))
-                {
-                    MyArray.InsertArrayElementAtIndex(MyArray.arraySize);
-                    MyArray.GetArrayElementAtIndex(MyArray.arraySize - 1).intValue = 0;
-                }
-
-                for (int a = 0; a < MyArray.arraySize; a++)
-                {
-                    EditorGUILayout.PropertyField(MyArray.GetArrayElementAtIndex(a));
-                    if (GUILayout.Button("Remove  (" + a.ToString() + ")", GUILayout.MaxWidth(100), GUILayout.MaxHeight(15)))
-                    {
-                        MyArray.DeleteArrayElementAtIndex(a);
-                    }
-                }
-            }
-            else
-            {
                 //Or
 
                 //2 : Full custom GUI Layout <-- Choose me I can be fully customized with GUI options.
                 EditorGUILayout.LabelField("Customizable Field With GUI");
-                MyGO.objectReferenceValue = EditorGUILayout.ObjectField("My Custom Go", MyGO.objectReferenceValue, typeof(GameObject), true);
-                MyInt.intValue = EditorGUILayout.IntField("My Custom Int", MyInt.intValue);
-                MyFloat.floatValue = EditorGUILayout.FloatField("My Custom Float", MyFloat.floatValue);
                 MyVect3.vector3Value = EditorGUILayout.Vector3Field("My Custom Vector 3", MyVect3.vector3Value);
 
 
@@ -163,7 +96,6 @@ public class WeaponEditor : Editor
             EditorGUILayout.Space();
             EditorGUILayout.Space();
             EditorGUILayout.Space();
-        }
 
         //Apply the changes to our list
         GetTarget.ApplyModifiedProperties();
